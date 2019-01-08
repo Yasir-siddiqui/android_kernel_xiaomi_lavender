9e7aa2e5a8b3fb8a9a60f79734e78fe7b389bfce
kdrag0n <dragon@khronodragon.com>
2019-01-07 18:59:35 -0800

    msm: kgsl: disable snapshot crash dump by default
    * Disable crash dump in init
    
    [@najahiiii] Disable crash dump in close
    
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>

--- drivers/gpu/msm/kgsl_snapshot.c
+++ drivers/gpu/msm/kgsl_snapshot.c
@@ -969,7 +969,7 @@ int kgsl_device_snapshot_init(struct kgsl_device *device)
 	device->snapshot = NULL;
 	device->snapshot_faultcount = 0;
 	device->force_panic = 0;
-	device->snapshot_crashdumper = 1;
+	device->snapshot_crashdumper = 0;
 
 	ret = kobject_init_and_add(&device->snapshot_kobj, &ktype_snapshot,
 		&device->dev->kobj, "snapshot");
@@ -1020,7 +1020,7 @@ void kgsl_device_snapshot_close(struct kgsl_device *device)
 	device->snapshot_memory.size = 0;
 	device->snapshot_faultcount = 0;
 	device->force_panic = 0;
-	device->snapshot_crashdumper = 1;
+	device->snapshot_crashdumper = 0;
 }
 EXPORT_SYMBOL(kgsl_device_snapshot_close);
 
